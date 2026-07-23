c71ad256792fd8794d3939fc792b734e3423ed07
CrystalAngel <rashid.mussin@gmail.com>
2026-07-24 04:23:30 +0500

    Implemented basic TCP server

---
+++ TcpServer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <winsock2.h>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+class TcpServer
+{
+
+private:
+    SOCKET serverSocket;
+    struct sockaddr_in address;
+    int port;
+
+public:
+    TcpServer(int port);
+    void start();
+};
--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include "WsaSession.h"
+#include "TcpServer.h"
 
-int main() {
-    try {
+int main()
+{
+    try
+    {
         std::cout << "Starting TCP Server..." << std::endl;
-        
-        WsaSession wsaSession; 
-        
 
-    } catch (const std::exception& e) {
+        WsaSession wsaSession;
+
+        TcpServer tcpServer(8808);
+        tcpServer.start();
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << "Fatal error: " << e.what() << std::endl;
         return 1;
     }
-    
+
     return 0;
 }
